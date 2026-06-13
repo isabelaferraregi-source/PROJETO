d97e71400056db56c7ea3bfc37c670e4dfb04202
isabelaferraregi-source <isabela.ferraregi@escola.pr.gov.br>
2026-06-13 19:38:07 -0300

    Add functions to display curiosity and scroll to top

---
+++ script.js
@@ -0,0 +1,15 @@
+function mostrarCuriosidade() {
+
+document.getElementById("curiosidade").innerHTML =
+"🌱 O Brasil é referência mundial no sistema de plantio direto, uma técnica que ajuda a conservar o solo e reduzir a erosão.";
+
+}
+
+function voltarAoTopo() {
+
+window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+});
+
+}
